c0903d785043a10eee30aecbfded8d8e20e9422c
Yewon Cho <qjqmti0113@gmail.com>
2024-10-30 20:35:57 +0900

    1030 cho - 로그인 화면 및 하단 탭 메뉴 수정, 메인 화면 수정중

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,22 +5,22 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="header">
-            <img src="/src/assets/img/basic_profile.png" alt="Profile Picture" className="profile-image" />
-            <div>
-                <h2 className="header-title">홍길동 - 0개월 12일 (만 0세)</h2>
-                <span className="header-dropdown">▼</span>
-                <Link
-                    to="/login"
-                    style={{
-                        fontSize: '12px',
-                        color: 'gray',
-                        position: 'absolute',
-                        top: '10px',
-                        right: '10px',
-                    }}
-                >
-                    로그인
-                </Link>
+            <div className='profile-wrap'>
+                <div className='profile-img-wrap'>
+                    <img src="/src/assets/img/basic_profile.png" alt="Profile Picture" className="profile-image" />
+                </div>
+                <div className='profile-info-wrap'>
+                    <span><b className='name'>홍길동</b>0개월 12일 (만 0세)<i class="bi bi-chevron-down"></i></span>
+                    <Link
+                        to="/login"
+                        style={{
+                            fontSize: '12px',
+                            color: 'gray',
+                            position: 'absolute',
+                            top: '10px',
+                            right: '10px',
+                        }}>로그인</Link>
+                </div>
             </div>
         </header>
     );
