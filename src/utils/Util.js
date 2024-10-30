4cb523ad81fc76d005e10a292221641413b4eae4
shAn-kor <kjn0406@naver.com>
2024-10-30 18:23:04 +0900

    shan 유효성검사 버그 수정

--- src/utils/Util.js
+++ src/utils/Util.js
@@ -4,3 +4,7 @@ export const getLastDateByMonth = (month, year) => {
     const daysInMonth = [31, checkLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     return daysInMonth[month-1];
 }
+
+export const numberAddZero = (value, length) => {
+    return String(value).padStart(length, "0");
+}
