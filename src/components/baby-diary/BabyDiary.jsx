1bce3ff05b8561b1657990ccc20dd06d350cae3e
Yewon Cho <qjqmti0113@gmail.com>
2024-11-07 16:45:09 +0900

    1107 cho - Header, Diary

--- src/components/baby-diary/BabyDiary.jsx
+++ src/components/baby-diary/BabyDiary.jsx
@@ -7,7 +7,7 @@ import imageEx03 from '../../assets/img/imageEx03.png';
 import imageEx04 from '../../assets/img/imageEx04.png';
 
 import './BabyDiary.css';
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 const BabyDiary = () => {
 
@@ -16,21 +16,22 @@ const BabyDiary = () => {
     }, []);
 
     return (
-        <>
-            <Calendar/>
+        <div className="diary-container">
+            <Calendar />
 
-            <div className="diary-entry">
-                <h3>10.11 첫 이유식 도전!</h3>
-                <p className="diary-text">오늘 드디어 아기 이유식을 시작했다. 쌀미음을 준비해서 작은 숟가락에 떠서 줬는데, 처음엔 어리둥절하더니 이내 찝찝거리면서 잘 먹었다.
-                    얼굴에 이유식을 잔뜩 묻히면서도 열심히 먹는 모습이 너무 귀여웠다. 아직은 양이 적...</p>
-                <div className="diary-images">
-                    <img src={imageEx01} alt="Image"/>
-                    <img src={imageEx02} alt="Image"/>
-                    <img src={imageEx03} alt="Image"/>
-                    <img src={imageEx04} alt="Image"/>
+            <div className="diary-wrap">
+                <div className="date">2024년 10월 11일 (금)</div>
+                <div className="title">첫 이유식 도전!</div>
+                <p className="text">오늘 드디어 아기 이유식을 시작했다. 쌀미음을 준비해서 작은 숟가락에 떠서 줬는데, 처음엔 어리둥절하더니 이내 찝찝거리면서 잘 먹었다.
+                    얼굴에 이유식을 잔뜩 묻히면서도 열심히 먹는 모습이 너무 귀여웠다.오늘 드디어 아기 이유식을 시작했다. 쌀미음을 준비해서 작은 숟가락에 떠서 줬는데, 처음엔 어리둥절하더니 이내 찝찝거리면서 잘 먹었다.
+                    얼굴에 이유식을 잔뜩 묻히면서도 열심히 먹는 모습이 너무 귀여웠다.오늘 드디어 아기 이유식을 시작했다. 쌀미음을 준비해서 작은 숟가락에 떠서 줬는데, 처음엔 어리둥절하더니 이내 찝찝거리면서 잘 먹었다.
+                    얼굴에 이유식을 잔뜩 묻히면서도 열심히 먹는 모습이 너무 귀여웠다.</p>
+                <div className="images">
+                    <img src={imageEx01} alt="Image" />
+                    <img src={imageEx02} alt="Image" />
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
