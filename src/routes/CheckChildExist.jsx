d7396291e09c0cf1c0b601b4055c7585cda30a12
Yewon Cho <qjqmti0113@gmail.com>
2024-11-08 14:45:10 +0900

    1108 cho - CheckChildExist PATHS 경로 수정

--- src/routes/CheckChildExist.jsx
+++ src/routes/CheckChildExist.jsx
@@ -6,7 +6,7 @@ const CheckChildExist = () => {
     const { childList } = useContext(AppContext);
 
     if (!childList || childList.length < 1) {
-        return <Navigate to={PATHS.CHILD_REGISTER} replace />;
+        return <Navigate to={PATHS.MY_PAGE.CHILD_REGISTER} replace />;
     }
 
     // 인증된 경우 자식 라우트 렌더링
