825d789f312697316951b08f88560b22d3f403ee
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2024-11-14 11:47:04 +0900

    1114kang

---
+++ src/components/ProgressBar/RiskLevelProgressBar.jsx
@@ -0,0 +1,20 @@
+import './RiskLevelProgressBar.css';
+
+const RiskLevelProgressBar = ({ riskLevel}) => {
+    return (
+        <div className="progress-container">
+            <div className="progress-bar-container">
+                <div className="progress-bar" style={{width: `${(riskLevel-1)*25}%`}}/>
+            </div>
+            <div className="progress-labels">
+                <span className="progress-label">아주 낮음</span>
+                <span className="progress-label">낮음</span>
+                <span className="progress-label">보통</span>
+                <span className="progress-label">높음</span>
+                <span className="progress-label">아주 높음</span>
+            </div>
+        </div>
+    );
+};
+
+export default RiskLevelProgressBar;
